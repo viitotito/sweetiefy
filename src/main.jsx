c899da5fd103835c9cc4ef28ad1a3eed24a8b121
viitotito <viitomontunes@gmail.com>
2025-11-23 13:27:28 -0300

    importado dependendicas

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './pages/App.jsx'
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"

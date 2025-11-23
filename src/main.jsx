acfefb07faf220cd10c3193364de7b8ad17ffa17
viitotito <viitomontunes@gmail.com>
2025-11-23 13:41:19 -0300

    modificado main com rotas de usuario

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,25 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { AuthProvider } from "./auth/AuthContext";
+
+import App from './pages/App.jsx'
+
+import UsuariosLogin from './pages/usuarios/UsuariosLogin.jsx';
+import UsuariosRegister from './pages/usuarios/UsuariosRegister.jsx';
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
 
 const router = createBrowserRouter([
     { path: "/", element: <App /> },
-    
-    { path: "/chamados", element: <ChamadosIndex /> },
-    { path: "/chamados/create", element: <ChamadosCreate /> },
-    { path: "/chamados/:id", element: <ChamadosShow /> },
-    { path: "/chamados/:id/edit", element: <ChamadosEdit /> },
 
     { path: "/usuarios/login", element: <UsuariosLogin /> },
     { path: "/usuarios/register", element: <UsuariosRegister /> },
 ]);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <AuthProvider>
+        <RouterProvider router={router} />
+    </AuthProvider>
 )
